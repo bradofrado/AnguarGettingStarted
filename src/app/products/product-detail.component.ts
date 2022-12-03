f017dc3f9cf9d88bd1f0a5092f3cc79973a82099
Braydon Jones <bradofrado@gmail.com>
2022-12-03 11:04:53 -0700

    Finished the course!

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+    pageTitle: string = 'Product Detail';
+    product: IProduct | undefined;
+    errorMessage: string = '';
+
+    constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
+
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        if (id) {
+            this.getProduct(id);
+        }
+    }
+
+    getProduct(id: Number): void {
+        this.productService.getProduct(id).subscribe({
+            next: product => this.product = product,
+            error: error => this.errorMessage = error
+        })
+    }
+
+    onBack(): void {
+        this.router.navigate(['/products']);
+    }
+
+}
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, map, Observable, tap, throwError } from "rxjs";
+import {IProduct} from './product';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    private productUrl = 'api/products/products.json';
+    
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    getProduct(id: Number) : Observable<IProduct | undefined> {
+        return this.getProducts().pipe(
+            map((products: IProduct[]) => products.find(p => p.productId === id))
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server to some remote loggin infrastructur
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly
+            errorMessage = `An error occured: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // the reponse body may contain clues as to what went wrong
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+
+        console.error(errorMessage);
+        return throwError(()=> errorMessage);
+    }
+}
